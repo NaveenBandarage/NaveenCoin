440abcd1aa93378f6de1627b4a6e37dd302753d9
Naveen Bandarage <thebanterage@gmail.com>
2020-06-28 00:31:37 +1200

    Added example block as well as new transaction method.

--- NaveenChain.py
+++ NaveenChain.py
@@ -22,3 +22,35 @@ class NaveenChain(object):
     def last_block(self):
         # Gets the last block
         pass
+    
+      def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+#example of what the block will look like. 
+# block = {
+#     'index': 1,
+#     'timestamp': 1506057125.900785,
+#     'transactions': [
+#         {
+#             'sender': "8527147fe1f5426f9dd545de4b27ee00",
+#             'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+#             'amount': 5,
+#         }
+#     ],
+#     'proof': 324984774000,
+#     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+# }
